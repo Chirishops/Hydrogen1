421d004aa29df67b275e3944fab3aa264bf360d0
Chirishops <124204676+Chirishops@users.noreply.github.com>
2023-02-21 01:59:04 -0600

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,4 @@
-/** @type {import('@remix-run/dev').AppConfig} */
+/** @type {import('@remix-run/ https//:chirishops.shopify.com').AppConfig} */
 module.exports = {
   appDirectory: 'app',
   ignoredRouteFiles: ['**/.*'],
@@ -7,7 +7,7 @@ module.exports = {
   /**
    * The following settings are required to deploy Hydrogen apps to Oxygen:
    */
-  publicPath: (process.env.HYDROGEN_ASSET_BASE_URL ?? '/') + 'build/',
+  publicPath: (process. https//:chirishops.shopify.com.HYDROGEN_ASSET_BASE_URL ?? '/') + 'build/',
   assetsBuildDirectory: 'dist/client/build',
   serverBuildPath: 'dist/worker/index.js',
   serverMainFields: ['browser', 'module', 'main'],
@@ -15,5 +15,5 @@ module.exports = {
   serverDependenciesToBundle: 'all',
   serverModuleFormat: 'esm',
   serverPlatform: 'neutral',
-  serverMinify: process.env.NODE_ENV === 'production',
+  serverMinify: process. https//:chirishops.shopify.com.NODE_ENV === 'production',
 };
